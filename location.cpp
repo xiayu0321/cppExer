a90231ccffcdaceefc96090b6c4f235b42fb72bd
xiayu0321 <xiayugogo0321@163.com>
2016-06-19 14:49:51 +0800

    modify

--- location.cpp
+++ location.cpp
@@ -11,23 +11,23 @@ class location{
 			x = m;
 			y = n;
 		}
-		
+
 		void init(int initx,int inity){
 			x = initx;
 			y = inity;
 		}
-		
+
 		int getx(){
 			return x;
 		}
-		
+
 		int gety(){
 			return y;
 		}
 };
 
 int main(){
-	// location A3;  error:no matching function for call to 'location::location()' 
+	// location A3;  error:no matching function for call to 'location::location()'
 	location A3(1,1);
 	cout<<A3.getx()<<" "<<A3.gety()<<endl;
 	A3.init(785,900);
